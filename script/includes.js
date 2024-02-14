3b98f6cdcdc4d63e7f853f9f04c173023cfa535a
Henrique_silva <henriquesilva9648@outlook.com>
2024-02-14 20:54:38 -0300

    Improved include function.

--- script/includes.js
+++ script/includes.js
@@ -4,13 +4,33 @@
     includes.forEach(async element => {
         const url = element.getAttribute("include");
         const replace = element.hasAttribute("replace");
+        const action = element.getAttribute("action");
 
         const request = await fetch(url);
-        const text = await request.text();
+        const data = await request.text();
+
+        switch(action) {
+            case "include":
+                element.innerHTML = data;
+            break;
+
+            case "insert_after":
+                element.insertAdjacentHTML("afterbegin", data);
+            break;
+
+            case "insert_before":
+                element.insertAdjacentHTML("beforeend", data);
+            break;
+
+            default:
+                element.innerHTML = data;
+            break;
+        }
         
-        replace ? element.innerHTML = text : element.insertAdjacentHTML("afterbegin", text);
+        // replace ? element.innerHTML = text : element.insertAdjacentHTML("afterbegin", text);
 
         element.removeAttribute("include");
         element.removeAttribute("replace");
+        element.removeAttribute("action");
     });
 } ())
